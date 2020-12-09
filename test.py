ec1d0073c9f30094a67823422cb5e69f0bc8f291
Shaun Kulesa <69469909+ShaunKulesa@users.noreply.github.com>
2020-12-09 20:25:08 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-from __future__ import print_function
-
-"""
-Demo entry point for Tkinter Window with OpenGL
-"""
+import time
+from tkinter import *
+from OpenGL import GL
+from pyopengltk import OpenGLFrame
 
 import sys, math, time
 if sys.version_info[0] < 3:
@@ -12,13 +11,9 @@ else:
 from OpenGL import GL, GLU
 from pyopengltk import OpenGLFrame
 
-import pygame
-
-
 class AppOgl(OpenGLFrame):
 
     def initgl(self):
-        GL.glShadeModel(GL_FLAT)
         GL.glViewport(0, 0, self.width, self.height)
         GL.glClearColor(0.0, 0.0, 0.0, 1.0)
         GL.glColor3f(1.0, 1.0, 1.0)
@@ -29,62 +24,32 @@ class AppOgl(OpenGLFrame):
         self.start = time.time()
         self.nframes = 0
 
-    def drawOneLine(self, x1, y1, x2, y2):
-      GL.glBegin(GL.GL_LINES)
-      GL.glVertex2f(x1, y1)
-      GL.glVertex2f(x2, y2)
-      GL.glEnd()
-
     def redraw(self):
-      GL.glClear(GL.GL_COLOR_BUFFER_BIT)
-
-      # in 1st row, 3 lines, each with a different stipple
-      GL.glEnable(GL.GL_LINE_STIPPLE)
-
-      GL.glLineStipple (1, 0x0101)  #  dotted
-      self.drawOneLine (50.0, 125.0, 150.0, 125.0)
-      GL.glLineStipple (1, 0x00FF)  #  dashed
-      self.drawOneLine (150.0, 125.0, 250.0, 125.0);
-      GL.glLineStipple (1, 0x1C47)  #  dash/dot/dash
-      self.drawOneLine (250.0, 125.0, 350.0, 125.0)
-
-      # in 2nd row, 3 wide lines, each with different stipple
-      GL.glLineWidth(5.0)
-      GL.glLineStipple(1, 0x0101)  #  dotted
-      self.drawOneLine(50.0, 100.0, 150.0, 100.0)
-      GL.glLineStipple(1, 0x00FF)  #  dashed
-      self.drawOneLine(150.0, 100.0, 250.0, 100.0)
-      GL.glLineStipple(1, 0x1C47)  #  dash/dot/dash
-      self.drawOneLine(250.0, 100.0, 350.0, 100.0)
-      GL.glLineWidth(1.0)
-
-      # in 3rd row, 6 lines, with dash/dot/dash stipple
-      # as part of a single connected line strip
-      GL.glLineStipple (1, 0x1C47)  # dash/dot/dash
-      GL.glBegin (GL.GL_LINE_STRIP)
-      for i in range(0, 7):
-        GL.glVertex2f(50.0 + (i * 50.0), 75.0)
-      GL.glEnd()
-
-      # in 4th row, 6 independent lines with same stipple  */
-      for i in range(0, 6):
-        self.drawOneLine (50.0 + (i * 50.0), 50.0, 50.0 + ((i+1) * 50.0), 50.0)
-
-      # in 5th row, 1 line, with dash/dot/dash stipple
-      # and a stipple repeat factor of 5
-      GL.glLineStipple (5, 0x1C47)  #  dash/dot/dash
-      self.drawOneLine (50.0, 25.0, 350.0, 25.0)
-
-      GL.glDisable (GL.GL_LINE_STIPPLE)
-      GL.glFlush ()
+        """Render a single frame"""
+        GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+        GL.glBegin(GL.GL_LINES)
+        GL.glVertex2f(10, 10)
+        GL.glVertex2f(20, 20)
+        GL.glEnd()
+        GL.glFlush()
         
+        
+        self.nframes += 1
+        tm = time.time() - self.start
+        
+        print("fps",self.nframes / tm, end="\r" )
 
-
+class StartPage(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master, width=750, height=400)
+        label = Label(root, text="hello")
+        label.pack()
 
 if __name__ == '__main__':
     root = Tk()
-    app = AppOgl(root, width=320, height=200)
+    app = AppOgl(root, width=750, height=400)
     app.pack(fill=BOTH, expand=YES)
+    ui = StartPage(root)
     app.animate = 1
     app.after(100, app.printContext)
     app.mainloop()
